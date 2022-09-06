86b2b104c5dffe9ca54f11e3f09f47e8b11b20b4
meftahmafazy <meftah.mafazy@gmail.com>
2022-09-06 10:35:28 +0800

    first init

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-danger ">
+    <div class="container">
+      <a class="navbar-brand" href="/">Kost Al-Hakim</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link {{ ($judul === "Home") ? 'active' : '' }}" href="/">Home</a>
+          <a class="nav-link {{ ($judul === "Home") ? 'active' : '' }}" href="/about">About</a>
+        </div>
+      </div>
+    </div>
+  </nav>
